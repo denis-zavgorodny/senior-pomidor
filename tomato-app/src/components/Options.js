4c1bc3a9916169648cfc665a9cc8ab4e7f700304
Denis Zavgorodny <deniszavgorodny@MacBook-Pro-Denis.local>
2018-01-03 18:19:40 +0200

    added validation form options

--- tomato-app/src/components/Options.js
+++ tomato-app/src/components/Options.js
@@ -19,34 +19,42 @@ class Options extends React.Component {
         return <div className="form-scroll"><form onSubmit={this.submit.bind(this)}>
             <div className="form-group ">
                 <label>Начало рбочего дня</label>
-                <input className="form-control" defaultValue={this.props.Options.start} name="start" onChange={this.fieldChange.bind(this)} />
+                <input required type="time" min="00:00" max="23:59" className="form-control" defaultValue={this.props.Options.start} name="start" onChange={this.fieldChange.bind(this)} />
+                <div className="form-error-text"><span class="icon icon-info-circled"></span>&nbsp;Вредено не верное время</div>
             </div>
             <div className="form-group ">
                 <label>Конец рбочего дня</label>
-                <input className="form-control" defaultValue={this.props.Options.end} name="end" onChange={this.fieldChange.bind(this)} />
+                <input required type="time" min="00:00" className="form-control" defaultValue={this.props.Options.end} name="end" onChange={this.fieldChange.bind(this)} />
+                <div className="form-error-text"><span class="icon icon-info-circled"></span>&nbsp;Вредено не верное время</div>
             </div>
             <div className="form-group ">
-                <label>Длина помидорки</label>
-                <input className="form-control" defaultValue={this.props.Options.interval} name="interval" onChange={this.fieldChange.bind(this)} />
+                <label>Длина помидорки, мин</label>
+                <input required type="number" min="1" max="60" className="form-control" defaultValue={this.props.Options.interval} name="interval" onChange={this.fieldChange.bind(this)} />
+                <div className="form-error-text"><span class="icon icon-info-circled"></span>&nbsp;Длина помидорки должна быть от 1 до 60 минут</div>
             </div>
             <div className="form-group ">
-                <label>Малый перерыв</label>
-                <input className="form-control" defaultValue={this.props.Options.break} name="break" onChange={this.fieldChange.bind(this)} />
+                <label>Малый перерыв, мин</label>
+                <input required type="number" min="1" max="10" className="form-control" defaultValue={this.props.Options.break} name="break" onChange={this.fieldChange.bind(this)} />
+                <div className="form-error-text"><span class="icon icon-info-circled"></span>&nbsp;Малый перерыв должен быть от 1 до 10 минут</div>
             </div>
             <div className="form-group ">
-                <label>Большой перерыв</label>
-                <input className="form-control" defaultValue={this.props.Options.breakLong} name="breakLong" onChange={this.fieldChange.bind(this)} />
+                <label>Большой перерыв, мин</label>
+                <input required type="number" min="5" max="30" className="form-control" defaultValue={this.props.Options.breakLong} name="breakLong" onChange={this.fieldChange.bind(this)} />
+                <div className="form-error-text"><span class="icon icon-info-circled"></span>&nbsp;Большой перерыв должен быть от 5 до 30 минут</div>
             </div>
             <div className="form-group ">
                 <label>Большой перерыв каждые</label>
-                <input className="form-control" defaultValue={this.props.Options.breakLongPeriod} name="breakLongPeriod" onChange={this.fieldChange.bind(this)} />
+                <input required type="number" min="2" max="10" className="form-control" defaultValue={this.props.Options.breakLongPeriod} name="breakLongPeriod" onChange={this.fieldChange.bind(this)} />
+                <div className="form-error-text"><span class="icon icon-info-circled"></span>&nbsp;Допустимые значения от 2 до 10</div>
             </div>
             <div className="form-group ">
                 <label>Перерыв на обед</label>
-                <input className="form-control" defaultValue={this.props.Options.lunch_from} name="lunch_from" onChange={this.fieldChange.bind(this)} />
+                <input required type="time" min="00:00" className="form-control" defaultValue={this.props.Options.lunch_from} name="lunch_from" onChange={this.fieldChange.bind(this)} />
+                <div className="form-error-text"><span class="icon icon-info-circled"></span>&nbsp;Вредено не верное время</div>
             </div>
             <div className="form-group ">
-                <input className="form-control" defaultValue={this.props.Options.lunch_to} name="lunch_to" onChange={this.fieldChange.bind(this)} />
+                <input required type="time" min="00:00" className="form-control" defaultValue={this.props.Options.lunch_to} name="lunch_to" onChange={this.fieldChange.bind(this)} />
+                <div className="form-error-text"><span class="icon icon-info-circled"></span>&nbsp;Вредено не верное время</div>
             </div>
 
             {/*<div className="checkbox">
