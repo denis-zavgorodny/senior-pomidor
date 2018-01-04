af5a953639e5847a2b10651fbe89ccfef6a7e381
Denis Zavgorodny <deniszavgorodny@MacBook-Pro-Denis.local>
2018-01-04 21:44:39 +0200

    add hight resolution icon

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ const emitter = new EventEmitter();
 let win;
 
 function createWindow () {
-    // Создаёт окно браузера.
     win = new BrowserWindow({
         backgroundColor: '#2e2c29',
         title: 'Senior pomodoro',
@@ -22,7 +21,6 @@ function createWindow () {
         icon: path.join(__dirname, '/assets/icon.png'),
     });
 
-    // и загрузит index.html приложение.
     win.loadURL(url.format({
         // pathname: process.env.NODE_ENV === 'development' ? 'localhost:3000/' : path.join(__dirname, 'tomato-app/build/index.html'),
         // protocol: process.env.NODE_ENV === 'development' ? 'http:' : 'file:',
@@ -31,36 +29,20 @@ function createWindow () {
         slashes: true
     }));
 
-
-    // Откроет DevTools.
-    // win.webContents.openDevTools();
-
-    // Возникает, когда окно будет закрыто.
     win.on('closed', () => {
-        // Разбирает объект окна, обычно вы можете хранить окна
-        // в массиве, если ваше приложение поддерживает несколько окон в это время,
-        // тогда вы должны удалить соответствующий элемент.
         win = null;
     });
 }
 
-// Этот метод будет вызываться, когда Electron закончит
-// инициализацию и готова к созданию окон браузера.
-// Некоторые интерфейсы API могут использоваться только после возникновения этого события.
 app.on('ready', createWindow);
 
-// Выйти, когда все окна будут закрыты.
 app.on('window-all-closed', () => {
-    // На macOS это обычно для приложений и их строки меню
-    // оставаться активным до тех пор, пока пользователь не выйдет явно с помощью Cmd + Q
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
 
 app.on('activate', () => {
-    // На MacOS это общее для того чтобы создать окно в приложении, когда значок
-    // dock нажали и нет других открытых окон.
     if (win === null) {
         createWindow();
     }
@@ -110,7 +92,3 @@ emitter.on('RUN_TIMER_PROXY', (store) => {
         lastState = currentState;
     }, 500);
 });
-
-
-// В этом файле вы можете включить код другого основного процесса
-// вашего приложения. Можно также поместить их в отдельные файлы и применить к ним require.
